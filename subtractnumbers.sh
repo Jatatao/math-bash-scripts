543a28366c34837f72194070c2d6cfb659951b26
Jatatao <72237218+Jatatao@users.noreply.github.com>
2021-02-15 17:22:20 -0700

    Add files via upload

---
+++ subtractnumbers.sh
@@ -0,0 +1,6 @@
+echo "Enter the first number that you would like to subtract"
+read u
+echo "Enter the second number that you would like to subtract"
+read i
+((answer=u-i))
+echo "The difference is $answer"
